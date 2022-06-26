080a73a7feef4b46a1e01d736b21b2a404a2c054
y-tetsu <45305605+y-tetsu@users.noreply.github.com>
2022-06-26 14:08:47 +0900

    step4

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 const EMPTY = 0;
+const O = 1;
+const X = 2;
 
+let turn = O;
 let board = Array(9).fill(EMPTY);
 
 function start() {
+  // 手番のテキストを更新する
+  document.getElementById('message').textContent = getTurnMessage(turn);
+
   // テーブルセルクリック時の処理を登録する
   for (let i=0; i<board.length; i++) {
     document.getElementById(i).addEventListener('click', onBoardClicked);
@@ -12,7 +18,15 @@ function start() {
 // テーブルセルをクリックした時の処理
 function onBoardClicked(event) {
   const index = Number(this.getAttribute('id'));
-  alert(index);
+
+  // 手番を回す
+  turn = turn === O ? X : O;
+  document.getElementById('message').textContent = getTurnMessage(turn);
+}
+
+// 手番のメッセージを取得
+function getTurnMessage(turn) {
+  return turn === O ? 'Oの番です' : 'Xの番です';
 }
 
 start();
