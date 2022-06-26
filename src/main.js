3b499cb07dd9cec4965f985f265db03fa1028c53
y-tetsu <45305605+y-tetsu@users.noreply.github.com>
2022-06-26 14:10:01 +0900

    step5

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 const EMPTY = 0;
 const O = 1;
 const X = 2;
+const IMAGES = {[O]: './image/o.png', [X]: './image/x.png'};
 
 let turn = O;
 let board = Array(9).fill(EMPTY);
@@ -18,6 +19,21 @@ function start() {
 // テーブルセルをクリックした時の処理
 function onBoardClicked(event) {
   const index = Number(this.getAttribute('id'));
+  if (board[index] !== EMPTY) return;
+  move(index);
+}
+
+// 一手打つ
+function move(index) {
+  // 盤面データを更新
+  board[index] = turn;
+
+  // テーブル画像の描画
+  const img = document.createElement('img');
+  img.src = IMAGES[turn];
+  const td = document.getElementById(index);
+  td.textContent = '';
+  td.appendChild(img);
 
   // 手番を回す
   turn = turn === O ? X : O;
